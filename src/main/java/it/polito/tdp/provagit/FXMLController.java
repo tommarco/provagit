083df48240e4273a10dcb9aa9ab6aa960b9b4e58
l4ffe <l4ffe@LAPTOP-T0J9VSTB>
2021-03-08 09:34:11 +0100

    Progetto modificato

--- src/main/java/it/polito/tdp/provagit/FXMLController.java
+++ src/main/java/it/polito/tdp/provagit/FXMLController.java
@@ -14,7 +14,7 @@ public class FXMLController implements Initializable {
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
+        System.out.println("Bottone premuto!");
         label.setText("Hello World!");
     }
     
